7203fc5913f7c8779f9b548e0f4a7ea56049147d
ICAZ117 <ibraheemcazalas@gmail.com>
2024-01-31 23:45:37 -0500

    Moved everything that was in main.dart to home.dart

---
+++ blend/lib/pages/home.dart
@@ -0,0 +1,296 @@
+import 'package:blend/pages/login.dart';
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Blend',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  // Method to get a new word pair
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritesPage();
+        break;
+      case 2:
+        page = PostingPagePlatforms();
+        break;
+      case 3:
+        // push to /login
+        // Navigator.pushNamed(context, '/login');
+        page = LoginPage();
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              //
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.cloud_circle_outlined),
+                  label: Text('Post'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.login),
+                  label: Text('Login'),
+                ),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                setState(() {
+                  print(value);
+                  selectedIndex = value;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: page,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PostDataState extends ChangeNotifier {
+  var platforms =
+      <String>[]; // a list of strings, each denoting the platforms to post to
+  var text = ""; // the text to go with the post
+  var media = ""; // pointer to the media that they provide
+}
+
+class PostingPagePlatforms extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // var appState = context.watch<PostDataState>();
+    // var cur_platforms = appState.platforms;
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MediaSelectionButton(
+            Icons.cloud,
+            "Instagram",
+          ),
+          Text("Text Item 2"),
+          Text("Text Item 3"),
+          Text("Text Item 4"),
+          Text("Text Item 5"),
+          Text("Text Item 6"),
+          Text("Text Item 7"),
+        ],
+      ),
+    );
+  }
+}
+
+class MediaSelectionButton extends StatelessWidget {
+
+  IconData buttonIcon;
+  String label;
+  MediaSelectionButton(this.buttonIcon, this.label);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Expanded(
+      child: ListView(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: ListTile(
+              leading: Icon(buttonIcon),
+              title: Text(label),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    //   return Scaffold(
+    //     body: Row(
+    //       children: [
+    //         Column(
+    //           children: [Icon(icon)],
+    //         ),
+    //         Column(
+    //           children: [Text(label)],
+    //         )
+    //       ],
+    //     ),
+    //   );
+    // }
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10), // Space out the text and the btn
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10), // Space out the btns
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: pair.asPascalCase,
+        ),
+      ),
+    );
+  }
+}
