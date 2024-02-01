c9c94d07dae6c65599b1e1115582354b72a720fe
Jvenecia <josven02@outlook.com>
2024-02-01 14:30:53 -0500

    More progress towards google authenticaiton sign in

--- blend/lib/pages/register.dart
+++ blend/lib/pages/register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -18,6 +19,31 @@ class _RegisterPageState extends State<RegisterPage> {
     confirmController.dispose();
     super.dispose();
   }
+
+  Future<UserCredential> signInWithGoogle() async {
+    // try{
+      print("google auth button SIGN IN WITH GOOGLE");
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser =
+          await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      return await FirebaseAuth.instance
+          .signInWithCredential(credential);
+  
+    } 
+  //   on Exception catch (e) {
+  //     print(e);
 
   @override
   Widget build(BuildContext context) {
@@ -56,8 +82,22 @@ class _RegisterPageState extends State<RegisterPage> {
             ElevatedButton(
               onPressed: () {
                 // Get values from text fields
+                print("Non-google sign up button pressed...");
               },
               child: Text('Sign Up'),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              // Google sign in button
+              onPressed: () async {
+                try {
+                  signInWithGoogle();
+                  print("works as expected...");
+                } on Exception catch (e) {
+                  print(e);
+                }
+              },
+              child: Text('Sign Up With Google'),
             ),
           ],
         ),
