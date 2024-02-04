d4fbde1a04a16c643c688bb3f2a196a37577bf21
Anthony Prancl <aprancl@mocs.flsouthern.edu>
2024-02-04 16:45:35 -0500

    can now read imaged from users, but still having trouble displaying. default image is in place until user chooses media.

--- blend/lib/pages/posting/postingState.dart
+++ blend/lib/pages/posting/postingState.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
 
 class PostDataState extends ChangeNotifier {
   Set<String> selectedPlatforms = {}; // a list of strings, each denoting the platforms to post to
   var text = ""; // the text to go with the post
-  var media = ""; // pointer to the media that they provide
+  var mediaPath = "images/lime.png"; // pointer to the media that they provide
 
-  void showSelectedPlatforms() {
-    print(selectedPlatforms);
-    notifyListeners();
 
+  Future pickImage() async {
+    try {
+      final img = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (img != null) {
+        mediaPath = img.path;
+        notifyListeners();
+      }
+    } on Error catch (err) {
+      debugPrint("Failed to find image: $err");
+    }
   }
-  
 }
