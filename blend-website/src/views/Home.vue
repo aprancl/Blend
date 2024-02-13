5792888b17f329b2405bfaed3c5e825622d9073e
ICAZ117 <ibraheemcazalas@gmail.com>
2024-02-12 23:34:13 -0500

    Created basic Vue website for ToS and Privacy Policy

---
+++ blend-website/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template lang="">
+    <div>
+        Blend is currently under development. Once it is ready for
+		production, this will be its home page!
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="">
+</style>
