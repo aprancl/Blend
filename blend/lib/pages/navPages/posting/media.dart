35faf3cd7d414c8a5aacfdd058738a63b6468027
Anthony Prancl <aprancl@mocs.flsouthern.edu>
2024-02-15 13:59:36 -0500

    added grid with user images, wich caveat that the user has to select them

--- blend/lib/pages/navPages/posting/media.dart
+++ blend/lib/pages/navPages/posting/media.dart
@@ -4,18 +4,45 @@ import 'package:provider/provider.dart';
 import 'package:blend/global_provider.dart';
 import 'package:image_picker/image_picker.dart';
 
-class PostingMediaPage extends StatelessWidget {
+class PostingMediaPage extends StatefulWidget {
+  @override
+  State<PostingMediaPage> createState() => _PostingMediaPageState();
+}
+
+class _PostingMediaPageState extends State<PostingMediaPage> {
+  List<File> galleryImages = [];
+  late GlobalProvider provider;
+
+  @override
+  void initState() {
+    super.initState();
+    provider = Provider.of<GlobalProvider>(context, listen: false);
+    loadGalleryImages();
+  }
+
+  Future<void> loadGalleryImages() async {
+    List<XFile>? images = await ImagePicker().pickMultiImage(
+      maxWidth: 500,
+      maxHeight: 500,
+      imageQuality: 80,
+    );
+
+    if (images != null) {
+      setState(() {
+        galleryImages = images.map((image) => File(image.path)).toList();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<GlobalProvider>(context);
-
     return Scaffold(
       body: SafeArea(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // ImageContainer(imgPath: provider.mediaFile.path),
+              // Your existing widgets
               ImageContainer(provider: provider),
               ElevatedButton(
                 child: Text('Add Media'),
@@ -46,6 +73,28 @@ class PostingMediaPage extends StatelessWidget {
                   ],
                 ),
               ),
+              Expanded(
+                child: GridView.builder(
+                  padding: EdgeInsets.only(top: 10, bottom: 30),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemCount: galleryImages.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: FileImage(galleryImages[index]),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
@@ -53,7 +102,76 @@ class PostingMediaPage extends StatelessWidget {
     );
   }
 }
+// class PostingMediaPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final provider = Provider.of<GlobalProvider>(context);
 
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               ImageContainer(provider: provider),
+//               ElevatedButton(
+//                 child: Text('Add Media'),
+//                 onPressed: () {
+//                   provider.selectImage();
+//                 },
+//               ),
+//               Container(
+//                 margin: EdgeInsets.symmetric(horizontal: 15.0),
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.end,
+//                   children: [
+//                     ElevatedButton(
+//                       child: Text('Back'),
+//                       onPressed: () {
+//                         print('We want to go back!');
+//                         provider.goToPage(1);
+//                       },
+//                     ),
+//                     Spacer(),
+//                     ElevatedButton(
+//                       child: Text('Next'),
+//                       onPressed: () {
+//                         print('We want to go next!');
+//                         provider.goToPage(4);
+//                       },
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//               Expanded(
+
+//                 child: GridView.builder(
+//                   padding: EdgeInsets.only(top: 10, bottom: 30, left: 10, right: 10),
+//                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//                     crossAxisCount: 3,
+//                     crossAxisSpacing: 10,
+//                     mainAxisSpacing: 10,
+//                   ),
+//                   itemCount: 12,
+//                   itemBuilder: (BuildContext context, int index) {
+//                     return Container(
+//                       decoration: BoxDecoration(
+//                         borderRadius: BorderRadius.circular(10),
+//                         image: DecorationImage(
+//                           image: AssetImage("images/lime.png"),
+//                         ),
+//                       ),
+//                     );
+//                   },
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class ImageContainer extends StatelessWidget {
   const ImageContainer({Key? key, required this.provider}) : super(key: key);
@@ -71,7 +189,7 @@ class ImageContainer extends StatelessWidget {
             fit: BoxFit.cover,
           )
         : Container(
-          padding: EdgeInsets.symmetric(horizontal: 10.0),
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
             child: Image.asset(
               provider.defaultImagePath,
             ),
