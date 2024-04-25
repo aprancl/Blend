199eba5cb6866c5bc3ae34bea7651070b2844b1b
Anthony Prancl <aprancl@mocs.flsouthern.edu>
2024-04-25 09:13:27 -0400

    auth appears to be working, but still have to press button to save changes, which is poor design. i will revisit if have time, but it is nonetheless functioning

--- blend/lib/pages/workspace/settings/web_view_container.dart
+++ blend/lib/pages/workspace/settings/web_view_container.dart
@@ -8,8 +8,7 @@ class WebViewContainer extends StatelessWidget {
   // Initialize webview controller
   final WebViewController controller = WebViewController()
     ..setJavaScriptMode(JavaScriptMode.disabled)
-    ..loadRequest(Uri.parse(
-        'https://www.linkedin.com/uas/login?session_redirect=%2Foauth%2Fv2%2Flogin-success%3Fapp_id%3D216614661%26auth_type%3DAC%26flow%3D%257B%2522authorizationType%2522%253A%2522OAUTH2_AUTHORIZATION_CODE%2522%252C%2522redirectUri%2522%253A%2522https%253A%252F%252Fwww.google.com%252F%2522%252C%2522scope%2522%253A%2522w_member_social%2522%252C%2522appId%2522%253A216614661%252C%2522authFlowName%2522%253A%2522generic-permission-list%2522%252C%2522currentSubStage%2522%253A0%252C%2522creationTime%2522%253A1714016869614%252C%2522currentStage%2522%253A%2522LOGIN_SUCCESS%2522%257D&fromSignIn=1&trk=oauth&cancel_redirect=%2Foauth%2Fv2%2Flogin-cancel%3Fapp_id%3D216614661%26auth_type%3DAC%26flow%3D%257B%2522authorizationType%2522%253A%2522OAUTH2_AUTHORIZATION_CODE%2522%252C%2522redirectUri%2522%253A%2522https%253A%252F%252Fwww.google.com%252F%2522%252C%2522scope%2522%253A%2522w_member_social%2522%252C%2522appId%2522%253A216614661%252C%2522authFlowName%2522%253A%2522generic-permission-list%2522%252C%2522currentSubStage%2522%253A0%252C%2522creationTime%2522%253A1714016869614%252C%2522currentStage%2522%253A%2522LOGIN_SUCCESS%2522%257D'));
+    ..loadRequest(Uri.parse(dotenv.env['linkedin_auth_link']!));
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +25,7 @@ class WebViewContainer extends StatelessWidget {
                 dotenv.env['linkedin_api_token'] = finalCode!;
                 Navigator.pop(context);
               },
-              icon: Icon(Icons.refresh))
+              icon: Icon(Icons.done))
         ],
       ),
       body: Container(
